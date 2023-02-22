4b73a59e8fd08f88d9bf158ce8565f2a6126724a
Adebiyi_Adeola_Helen <adeolahelen2604@gmail.com>
2023-02-22 03:40:01 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet 10x
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int n = 0;
+	char i = 'a';
+
+	while (n < 10)
+	{
+		i = 'a';
+		while (i <= 'z')
+		{
+			_putchar(i);
+			i++;
+		}
+		_putchar('n');
+		n++;
+	}
+}
