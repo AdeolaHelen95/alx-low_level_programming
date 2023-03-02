c18ddd3cc0cd4aceae8153e4c8eaa790d1e9515e
Adebiyi_Adeola_Helen <adeolahelen2604@gmail.com>
2023-03-02 12:28:23 -0800

    2-strncpy.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -18,7 +18,7 @@ char *_strncat(char *dest, char *src, int n)
 		len++;
 	}
 
-	for (i = 0 ; i < len && src[i] != '\0' ; i++)
+	for (i = 0 ; i < n && src[i] != '\0' ; i++)
 		dest[len + i] = src[i];
 	dest[len + i] = '\0';
 
