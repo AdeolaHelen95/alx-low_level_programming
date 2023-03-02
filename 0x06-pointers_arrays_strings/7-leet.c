d2b6662330bb168f5cdbf7e83ba788b1c51b0f05
Adebiyi_Adeola_Helen <adeolahelen2604@gmail.com>
2023-03-02 13:11:22 -0800

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,28 +1,32 @@
 #include "main.h"
 
 /**
- * leet - function that encodes a string into 1337
- * @s: string array
- * Return: string array
+ * leet - Entry point
+ * @str: Pointer to character array.
+ *
+ * This encodes a string into 1337 (leet).
+ *
+ * Return: Pointer to string.
  */
 
-char *leet(char *s)
+char *leet(char *str)
 {
-	int i;
+	int c = 0;
+	int u;
+	char leet_chars[] = "aAeEoOtTlL";
+	char leet_nums[] = "4433007711";
 
-	for (i = 0; s[i] != '\0'; i++)
+	/* Capitalize the remaining words */
+	while (str[c] != '\0')
 	{
-		if (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		if (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		if (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		if (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		if (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
+		for (u = 0; u < 10; u++)
+		{
+			if (leet_chars[u] == str[c])
+			{
+				str[c] = leet_nums[u];
+			}
+		}
+		c++;
 	}
-
-	return (s);
+	return (str);
 }
